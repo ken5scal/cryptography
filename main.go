8b8b35a5a22c9f4694b155a4c5237a827efd4110
ken5scal <kengoscal@gmail.com>
2017-07-25 09:08:37 +0900

    fix

--- main.go
+++ main.go
@@ -3,38 +3,42 @@ package main
 import (
 	"math/big"
 	"fmt"
+	"errors"
 )
 
 func main() {
-	g := Gcd(big.NewInt(780), big.NewInt(660))
+	g, _ := Gcd(big.NewInt(2), big.NewInt(10))
 	fmt.Println(g)
 }
 
 
-func Gcd(a, b *big.Int) *big.Int {
+func Gcd(a, b *big.Int) (*big.Int, error) {
+	if a.Sign() <= 0 || b.Sign() <= 0 {
+		return nil, errors.New("Input must be positive number")
+	}
+
 	g := big.NewInt(1)
 	if a.Cmp(b) < 0 {
 		tmp := a; a = b; b= tmp
 	}
 
 	n := big.NewInt(2)
-	for n.Cmp(b) < 0 {
-		for  {
-			var tmpA big.Int
-			var tmpB big.Int
+	var tmpA big.Int
+	var tmpB big.Int
 
+	for n.Cmp(b) <= 0 {
+		for  {
 			if tmpA.Mod(a, n).Cmp(big.NewInt(0)) == 0 &&
 				tmpB.Mod(b, n).Cmp(big.NewInt(0)) == 0 {
-				g = g.Mul(g, n)
-				a = a.Div(a, n)
-				b = b.Div(b, n)
-
+				g.Mul(g, n)
+				a.Div(a, n)
+				b.Div(b, n)
 			} else {
 				break
 			}
 		}
-		n = n.Add(n, big.NewInt(1))
+		n.Add(n, big.NewInt(1))
 	}
 
-	return g
+	return g, nil
 }
