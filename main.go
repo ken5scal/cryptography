a38ef355540edc7be0f7484bd9c511d078eed05e
ken5scal <kengoscal@gmail.com>
2017-07-25 09:13:33 +0900

    Reduce one var

--- main.go
+++ main.go
@@ -11,25 +11,25 @@ func main() {
 	fmt.Println(g)
 }
 
-
+// 試行 割り算
 func Gcd(a, b *big.Int) (*big.Int, error) {
 	if a.Sign() <= 0 || b.Sign() <= 0 {
 		return nil, errors.New("Input must be positive number")
 	}
 
+	var tmp big.Int
 	g := big.NewInt(1)
 	if a.Cmp(b) < 0 {
-		tmp := a; a = b; b= tmp
+		tmp := a
+		a = b
+		b = tmp
 	}
 
 	n := big.NewInt(2)
-	var tmpA big.Int
-	var tmpB big.Int
-
 	for n.Cmp(b) <= 0 {
 		for  {
-			if tmpA.Mod(a, n).Cmp(big.NewInt(0)) == 0 &&
-				tmpB.Mod(b, n).Cmp(big.NewInt(0)) == 0 {
+			if tmp.Mod(a, n).Cmp(big.NewInt(0)) == 0 &&
+				tmp.Mod(b, n).Cmp(big.NewInt(0)) == 0 {
 				g.Mul(g, n)
 				a.Div(a, n)
 				b.Div(b, n)
