5f19a95c12567cb37d366817e36b319f5b80daca
ken5scal <kengoscal@gmail.com>
2017-08-05 17:37:37 +0900

    First time implementing benchmark

--- main_test.go
+++ main_test.go
@@ -11,6 +11,7 @@ var b = big.NewInt(15000)
 var expected = big.NewInt(8)
 
 func TestGcd(t *testing.T) {
+
 	actual, _ := Gcd(a, b)
 	if expected.Cmp(actual) != 0 {
 		t.Error(fmt.Sprintf("expected %v, but got %v", expected, actual))
@@ -18,6 +19,9 @@ func TestGcd(t *testing.T) {
 }
 
 func TestBinaryEuclidGCD(t *testing.T) {
+	var a = big.NewInt(1024)
+	var b = big.NewInt(15000)
+	var expected = big.NewInt(8)
 	actual, _ := BinaryEuclidGCD(a, b)
 	if expected.Cmp(actual) != 0 {
 		t.Error(fmt.Sprintf("expected %v, but got %v", expected, actual))
@@ -25,14 +29,22 @@ func TestBinaryEuclidGCD(t *testing.T) {
 }
 
 func TestBadBinaryEuclidGCD(t *testing.T) {
-	a = big.NewInt(-1)
-	actual, err := BinaryEuclidGCD(a, b)
+	var b = big.NewInt(15000)
+	badA := big.NewInt(-1)
+	actual, err := BinaryEuclidGCD(badA, b)
 	if actual != nil && err == nil {
 		t.Error("actual must be null value. Error must not be a null value")
 	}
 }
 
-func BenchmarkBinaryEuclidGCD(bench *testing.B) {
-	bench.ResetTimer()
-	BinaryEuclidGCD(a,b)
+func BenchmarkGcd_normal(bench *testing.B) {
+	for i := 0; i < 10000; i++ {
+		Gcd(a, b)
+	}
+}
+
+func BenchmarkGCD_binaryEuclid(bench *testing.B) {
+	for i := 0; i < 10000; i++ {
+		BinaryEuclidGCD(a, b)
+	}
 }
