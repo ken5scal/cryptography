fc615bda63358bca953daa77f71f9813340d2f07
ken5scal <kengoscal@gmail.com>
2017-08-06 12:46:39 +0900

    Wrote a Test

--- modular_exponential_calculation.go
+++ modular_exponential_calculation.go
@@ -4,8 +4,23 @@ import (
 	"math/big"
 	"errors"
 )
+/*
+RSA Encryption/Decryption is done by Modular Exponential Calculation
+S = a^m mod N
+ Encryption:
+  - S: Cipher Text
+  - a: Plain Text
+  - m: Public Exponent公開指数(e)
+  - N: Public Modulus(N)
+ Decryption:
+  - S: Plain Text
+  - a: Cipher Text
+  - m: Secret Exponent秘密指数(d)
+  - N: Public Modulus(N)
+*/
 
-func ModPow(a, N, m big.Int) (S *big.Int, err error) {
+// ModPow calculates the most fundamental binary function.
+func ModPow(a, N, m *big.Int) (S *big.Int, err error) {
 	if N.Sign() <= 0 || m.Sign() <= 0 {
 		return S, errors.New("Input must be positive number")
 	}
@@ -13,9 +28,9 @@ func ModPow(a, N, m big.Int) (S *big.Int, err error) {
 	S = big.NewInt(1)
 
 	for j := m.BitLen() - 1; j >= 0; j-- {
-		S.ModSqrt(S, &N)
+		S.ModSqrt(S, N)
 		if m.Bit(j) == 1 {
-			S.Div(S, &a).Mod(S, &N)
+			S.Div(S, a).Mod(S, N)
 		}
 	}
 	return S, nil
--- modular_exponential_calculation_test.go
+++ modular_exponential_calculation_test.go
@@ -7,13 +7,17 @@ import (
 )
 
 func TestModPow(t *testing.T) {
-	a := big.NewInt(2048)
-	m := big.NewInt(10)
-	n := big.NewInt(6)
+	a := big.NewInt(10)
+	m := big.NewInt(13)
+	n := big.NewInt(19)
+	expectedS := a.Exp(a, m, n)
 
-	expectedS := big.NewInt(7148)
-	s := ModPow(*a,*m,*n)
-	if expectedS.Cmp(&s) != 0 {
+	s, err := ModPow(a,m,n)
+	if err != nil {
+		t.Errorf("Error: %v\n", err)
+		return
+	}
+	if expectedS.Cmp(s) != 0 {
 		t.Error(fmt.Sprintf("expected %v, but got %v", expected, s))
 	}
 }
