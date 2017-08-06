ded14ec361dd7174dac3965630bce02c7efc57e6
ken5scal <kengoscal@gmail.com>
2017-08-06 12:29:21 +0900

    Add error statement

--- modular_exponential_calculation.go
+++ modular_exponential_calculation.go
@@ -1,15 +1,22 @@
 package main
 
-import "math/big"
+import (
+	"math/big"
+	"errors"
+)
 
-func ModPow(a, N big.Int, m []big.Int) (S *big.Int) {
+func ModPow(a, N, m big.Int) (S *big.Int, err error) {
+	if N.Sign() <= 0 || m.Sign() <= 0 {
+		return S, errors.New("Input must be positive number")
+	}
+
 	S = big.NewInt(1)
-	for j := N.Int64() - 1; j >= 0; j-- {
-		S.ModSqrt(S, &N)
 
-		if m[j].Int64() == 1 {
+	for j := m.BitLen() - 1; j >= 0; j-- {
+		S.ModSqrt(S, &N)
+		if m.Bit(j) == 1 {
 			S.Div(S, &a).Mod(S, &N)
 		}
 	}
-	return S
+	return S, nil
 }
