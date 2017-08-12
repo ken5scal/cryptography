64cfcee9e44c12360659c7e48ba1e39cc389d4dd
ken5scal <kengoscal@gmail.com>
2017-08-12 13:42:30 +0900

    Remove unnecessary lines

--- main.go
+++ main.go
@@ -1,28 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"math/big"
-)
-
 func main() {
-	a := big.NewInt(780)
-	b := big.NewInt(600)
-	g, _ := Gcd(a, b)
-	fmt.Println(g)
-
-	a = big.NewInt(135632)
-	b = big.NewInt(44461)
-	g = EuclidGCD(a, b)
-	fmt.Println(g)
-
-	a = big.NewInt(1024)
-	b = big.NewInt(15000)
-	g, _ = BinaryEuclidGCD(a, b)
-	fmt.Println(g)
-
-	a = big.NewInt(2793)
-	b = big.NewInt(828)
-	x, y, g, _ := extendedEuclidGCD(a, b)
-	fmt.Println(x, y, g)
 }
