74734bc87522b92b63b197d107de992a150994e5
ken5scal <kengoscal@gmail.com>
2017-08-15 09:16:37 +0900

    First commit for Probabilistic primary test using fermat theory

---
+++ probabilistic_primary.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"math/big"
+	"errors"
+)
+
+func IsPrimeByFermat(r *big.Int, t *big.Int) (bool, error) {
+	return false, errors.New("something")
+}
---
+++ probabilistic_primary_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"testing"
+	"math/big"
+)
+
+func TestIsPrimeByFermat(t *testing.T) {
+	r := big.NewInt(7)
+	t2 := big.NewInt(1   )
+	actual, err := IsPrimeByFermat(r, t2)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !actual {
+		t.Errorf("Expected r %v to be prime, but not.\n", r)
+	}
+}
+
+func TestIllegalArgumentPrimeByFermat(t *testing.T) {
+	r := big.NewInt(0)
+	t2 := big.NewInt(1)
+
+	_, err := IsPrimeByFermat(r, t2)
+
+	if err != nil {
+		t.Errorf("Expected to return error due to illegal argument.")
+	}
+}
