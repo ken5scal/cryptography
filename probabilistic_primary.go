bf4870b01fa26932d40d7ae660a3da72fe40d6de
ken5scal <kengoscal@gmail.com>
2017-08-23 09:19:29 +0900

    update

--- probabilistic_primary.go
+++ probabilistic_primary.go
@@ -6,18 +6,21 @@ import (
 	"math/rand"
 )
 
-var one = big.NewInt(1)
+var bigOne = big.NewInt(1)
+var bigThree = big.NewInt(3)
 
 func IsPrimeByFermat(r *big.Int, t *big.Int) (bool, error) {
 
-	if r.Cmp(big.NewInt(3)) == -1 || t.Cmp(one) == -1 {
+	if r.Cmp(bigThree) == -1 || t.Cmp(bigOne) == -1 {
 		return false, errors.New("r must be larger than or equal to 3, and t must be larger than or equal to 1")
 	}
+
 	a := big.NewInt(rand.Int63n(r.Int64()) - 1)
 	S := new(big.Int)
-	for j := 1; j >= int(t.Int64()); j++ {
-		S.Exp(a, S.Sub(r, one), r)
-		if S.Cmp(one) != 0 {
+
+	for j := 1; j < int(t.Int64()); j++ {
+		S.Exp(a, S.Sub(r, bigOne), r)
+		if S.Cmp(bigOne) != 0 {
 			return false, nil
 		}
 	}
