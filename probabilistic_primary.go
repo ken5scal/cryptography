7e4b86c78d97f62cf3a4d6ae1b5115680ffcd7ab
ken5scal <kengoscal@gmail.com>
2017-08-23 09:28:27 +0900

    comment

--- probabilistic_primary.go
+++ probabilistic_primary.go
@@ -9,16 +9,21 @@ import (
 var bigOne = big.NewInt(1)
 var bigThree = big.NewInt(3)
 
+// IsPrimeByFermat judges whether r is prime number using Probabilistic Prime Test algorithm, Fermat
+// t is usually 1.
+// Down side is that the test does not quantify a probability of r being prime number.
+// It just repeats t times to check {a^(p-1) mod p} is 1  (a ^(p-1) - 1 is multiple of p)
 func IsPrimeByFermat(r *big.Int, t *big.Int) (bool, error) {
 
 	if r.Cmp(bigThree) == -1 || t.Cmp(bigOne) == -1 {
 		return false, errors.New("r must be larger than or equal to 3, and t must be larger than or equal to 1")
 	}
 
-	a := big.NewInt(rand.Int63n(r.Int64()) - 1)
 	S := new(big.Int)
 
 	for j := 1; j < int(t.Int64()); j++ {
+		// Pre-assumption is that a is universally random.
+		a := big.NewInt(rand.Int63n(r.Int64()) - 1)
 		S.Exp(a, S.Sub(r, bigOne), r)
 		if S.Cmp(bigOne) != 0 {
 			return false, nil
