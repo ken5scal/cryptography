4c02f40de0b6ac91a15b1a4f43522875bb5de999
ken5scal <kengoscal@gmail.com>
2017-09-02 23:49:50 +0900

    Numerous attempt

--- aes/aes.go
+++ aes/aes.go
@@ -1,5 +1,9 @@
 package aes
 
+import (
+	"errors"
+	"fmt"
+)
 
 /*
 # State
@@ -7,9 +11,11 @@ A 4 × 4 column-major order matrix of bytes on where AES operates.
 128 bits (16 bytes) of AES input is placed in each cell(1 byte).
  */
 
-
-func setStateBlock(buffer []byte, offset int) {
-
+func setStateBlock(buffer []byte, offset int) error {
+	if buffer != nil || offset < 0 || len(buffer) < offset + 16 {
+		return errors.New("Illegal Argument Exception")
+	}
+	return nil
 }
 
 func getStateBlock(buffer []byte, offset int) {
--- main.go
+++ main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+	"bytes"
+)
+
 func main() {
+	ff := "hogehoge"
+	fmt.Println(reflect.TypeOf(ff))
+	fmt.Printf("%b", ff)
+
+	var buffer bytes.Buffer
+	for i := 0; i < len(ff);  i++ {
+		fmt.Fprintf(&buffer, "%b", ff[i])
+	}
+
+	fmt.Println([]byte(ff))
+
 }
