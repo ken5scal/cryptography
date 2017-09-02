ad41b8207fdca5044bec0d9c7a1b210f2dfa6b9e
ken5scal <kengoscal@gmail.com>
2017-09-03 00:58:32 +0900

    Implemented state block

--- aes/aes.go
+++ aes/aes.go
@@ -2,7 +2,7 @@ package aes
 
 import (
 	"errors"
-	"fmt"
+
 )
 
 /*
@@ -11,11 +11,17 @@ A 4 × 4 column-major order matrix of bytes on where AES operates.
 128 bits (16 bytes) of AES input is placed in each cell(1 byte).
  */
 
-func setStateBlock(buffer []byte, offset int) error {
+func setStateBlock(buffer []byte, offset int) (state []uint32, err error) {
 	if buffer != nil || offset < 0 || len(buffer) < offset + 16 {
-		return errors.New("Illegal Argument Exception")
+		return nil, errors.New("Illegal Argument Exception")
 	}
-	return nil
+
+	state = make([]uint32, 4)
+	state[0] = uint32(buffer[0])<<24 | uint32(buffer[1])<<16 | uint32(buffer[2])<<8 | uint32(buffer[3])
+	state[1] = uint32(buffer[4])<<24 | uint32(buffer[5])<<16 | uint32(buffer[6])<<8 | uint32(buffer[7])
+	state[2] = uint32(buffer[8])<<24 | uint32(buffer[9])<<16 | uint32(buffer[10])<<8 | uint32(buffer[11])
+	state[3] = uint32(buffer[12])<<24 | uint32(buffer[13])<<16 | uint32(buffer[14])<<8 | uint32(buffer[15])
+	return
 }
 
 func getStateBlock(buffer []byte, offset int) {
--- main.go
+++ main.go
@@ -22,11 +22,10 @@ func main() {
 	//s2 = uint32(src[8])<<24 | uint32(src[9])<<16 | uint32(src[10])<<8 | uint32(src[11])
 	//s3 = uint32(src[12])<<24 | uint32(src[13])<<16 | uint32(src[14])<<8 | uint32(src[15])
 
-	fmt.Println(src[0], fmt.Sprintf("%b\n",src[0]))
-	fmt.Println(src[0]<<2)
-	fmt.Println(uint32(src[0]<<24))
-	fmt.Println(uint32(src[0]<<24))
-	fmt.Println(uint32(src[0]<<24))
-	fmt.Println(uint32(src[0]<<24))
+	fmt.Println(uint32(src[3]), fmt.Sprintf("%b",uint32(src[3])))
+	fmt.Println(uint32(src[2])<<8, fmt.Sprintf("%b",uint32(src[2])<<8))
+	fmt.Println(uint32(src[1])<<16, fmt.Sprintf("%b",uint32(src[1])<<16))
+	fmt.Println(uint32(src[0])<<24, fmt.Sprintf("%b",uint32(src[0])<<24))
+	fmt.Println(fmt.Sprintf("%b", uint32(src[0])<<24 | uint32(src[1])<<16 | uint32(src[2])<<8 | uint32(src[3])))
 	fmt.Println(s0)
 }
